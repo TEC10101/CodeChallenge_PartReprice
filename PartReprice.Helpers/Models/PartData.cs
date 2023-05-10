7e3f3e5fef57093aeb0b03970bba6f4ce9348438
Tyler Corum <22278328+TEC10101@users.noreply.github.com>
2023-05-10 15:35:00 -0600

    Basic form and unit tests, need to fill out logic, building successfully

---
+++ PartReprice.Helpers/Models/PartData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartReprice.Helpers.Models
+{
+    internal class PartData
+    {
+        public int PartId { get; set; }
+        public string PartDesc { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- PartReprice/Form1.cs
+++ PartReprice/Form1.cs
@@ -6,5 +6,26 @@ namespace PartReprice
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+        }
     }
 }
